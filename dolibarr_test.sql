86baf9d54681d7f739b748b2f14f315fcb10bbb0
root <nicolasstoupy@gmail.com>
2024-05-27 02:00:17 +0000

    Backup effectué le 2024-05-27 02:00:17

--- dolibarr_test.sql
+++ dolibarr_test.sql
@@ -8643,4 +8643,4 @@ CREATE TABLE `llxna_usergroup_user` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-05-26  2:00:11
+-- Dump completed on 2024-05-27  2:00:12
