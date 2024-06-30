480a83f1e4fba25182b24704e5edfcba78e55054
root <nicolasstoupy@gmail.com>
2024-06-30 02:00:18 +0000

    Backup effectué le 2024-06-30 02:00:18

--- tempodol.sql
+++ tempodol.sql
@@ -8545,4 +8545,4 @@ CREATE TABLE `llxna_usergroup_user` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-06-29  2:00:15
+-- Dump completed on 2024-06-30  2:00:17
